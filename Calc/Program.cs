288c9b512c951d52bb7aef17a45706db596a86fc
jdani <jdani@NANO-NUCLEAR>
2023-07-26 13:31:24 -0600

    Agregar archivos de proyecto.

---
+++ Calc/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+double res = 0;
+void operaAB(double r, double s, char operador) 
+{ 
+
+    switch (operador)
+    {
+        case '+':
+            res = r + s;
+            break;
+        case '-':
+            res = r - s;
+            break;
+        case '*':
+            res = r * s;
+            break;
+        case '/':
+            res = r / s;
+            break;
+
+    }
+
+    Console.WriteLine("El resultado es!!!!! " + res);
+}
+
+char operacion;
+double A;
+double B;
+char[] operadores = { '+', '-', '*', '/' };
+
+Console.WriteLine("Que operacion deseas?");
+operacion = Console.ReadLine().ToCharArray()[0];
+
+if (operadores.Contains(operacion)) {
+    Console.WriteLine("Que numeros vas a operar?");
+    A = Convert.ToDouble(Console.ReadLine());
+    B = Convert.ToDouble(Console.ReadLine());
+
+    operaAB(A, B, operacion);
+} else {
+    Console.WriteLine("Eso no es ningun operador");
+}
+
+
+
+
+
